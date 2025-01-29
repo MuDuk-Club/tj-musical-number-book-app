091c3ee9a38bde012fa2f04d3b26468f20cc723f
mic050r <s2216@e-mirim.hs.kr>
2025-01-30 01:02:07 +0900

    ✨ Feat(#4) :: 오늘 부를 넘버 페이지 > 추가한 값 불러오기/삭제, 순서 업데이트 기능 구현
     - 오늘 부를 넘버 페이지 UI 구현
     - Card 드래그 기능 구현
     - SharedPreferences 값 불러오기 구현
     - SharedPreferences 값 삭제하기 구현
     - Card 드래그 > 순서 변경시 SharedPreferences 값 업데이트 되도록 구현

--- lib/services/song_service.dart
+++ lib/services/song_service.dart
@@ -13,3 +13,50 @@ Future<void> saveSong(Song song) async {
   savedSongsJson.add(songJson);
   await prefs.setStringList('saved_songs', savedSongsJson);
 }
+
+// SharedPreferences에서 저장된 Song 객체를 불러오는 함수
+Future<List<Song>> getSavedSongs() async {
+  final prefs = await SharedPreferences.getInstance();
+
+  // SharedPreferences에서 저장된 노래 목록을 가져옴
+  List<String>? songsJson = prefs.getStringList('saved_songs');
+
+  if (songsJson != null) {
+    // JSON 문자열을 Song 객체 리스트로 변환
+    return songsJson.map((songJson) {
+      return Song.fromJson(jsonDecode(songJson));
+    }).toList();
+  }
+
+  return [];
+}
+
+// 노래 삭제하기
+Future<void> deleteSong(Song song) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  List<String>? songsJson = prefs.getStringList('saved_songs');
+
+  if (songsJson != null) {
+    // 노래 리스트에서 삭제할 노래를 제거
+    songsJson.removeWhere((songData) {
+      Song s = Song.fromJson(json.decode(songData));
+      return s.tjNumber == song.tjNumber; // tjNumber를 기준으로 삭제
+    });
+
+    // 삭제된 리스트를 다시 저장
+    await prefs.setStringList('saved_songs', songsJson);
+  }
+}
+
+// 노래 순서 업데이트
+Future<void> updateSongs(List<Song> songs) async {
+  // SharedPreferences 인스턴스 가져오기
+  final prefs = await SharedPreferences.getInstance();
+
+  // Song 객체를 JSON 문자열로 변환
+  List<String> songsJson =
+      songs.map((song) => jsonEncode(song.toJson())).toList();
+
+  // SharedPreferences에 저장
+  await prefs.setStringList('saved_songs', songsJson);
+}
